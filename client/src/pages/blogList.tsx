42792259ae0e1209b43e93fa0246f68739ee9012
unknown <jabezayugu04@gmail.com>
2025-07-09 22:27:32 +0300

    Authentication

---
+++ client/src/pages/blogList.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material"
+
+const BlogList: React.FC = () => {
+    return(
+        <>
+        <Typography variant="h1">Recent Blog Post</Typography>
+        </>
+    )
+}
+export default BlogList
